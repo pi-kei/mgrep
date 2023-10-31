175c07713f2764ddc9361f21eacc60165e370da0
pavel <kim110488@gmail.com>
2023-10-31 03:20:18 +0300

    Add sink package tests

---
+++ internal/sink/writer_test.go
@@ -0,0 +1,37 @@
+package sink
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/pi-kei/mgrep/internal/base"
+)
+
+func TestWriterSink_HandlerResult(t *testing.T) {
+	var sb strings.Builder
+	sink := NewWriterSink(&sb)
+
+	sink.HandleResult(base.SearchResult{Path: "a/b/c.txt", LineNumber: 1, StartIndex: 2, EndIndex: 5, Line: "test test"})
+	out := sb.String()
+	if out != "a/b/c.txt[1,3]:test test\n" {
+		t.Errorf("Invalid output: %s", out)
+	}
+}
+
+func TestCustomWriterSink_HandlerResult(t *testing.T) {
+	var sb strings.Builder
+	calledTimes := 0
+	sink := NewCustomWriterSink(&sb, "%s %v %v %v %s", func(result base.SearchResult) []any {
+		calledTimes++
+		return []any{result.Path, result.LineNumber, result.StartIndex, result.EndIndex, result.Line}
+	})
+
+	sink.HandleResult(base.SearchResult{Path: "a/b/c.txt", LineNumber: 1, StartIndex: 2, EndIndex: 5, Line: "test test"})
+	out := sb.String()
+	if out != "a/b/c.txt 1 2 5 test test" {
+		t.Errorf("Invalid output: %s", out)
+	}
+	if calledTimes != 1 {
+		t.Errorf("Called times %v", calledTimes)
+	}
+}
